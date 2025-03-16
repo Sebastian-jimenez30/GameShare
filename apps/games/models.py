5b80f7e2c2a678b20265569109fed60093726dd6
jmmunozg1 <jmmunozg1@eafit.edu.co>
2025-03-16 16:54:53 -0500

    Add Models in games

--- apps/games/models.py
+++ apps/games/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from apps.users.models import User
 
-# Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.category_name
+
+class Game(models.Model):
+    title = models.CharField(max_length=100)
+    developer = models.CharField(max_length=100)
+    year = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    image = models.ImageField(upload_to='static/media', null=True, blank=True)
+
+    category = models.ManyToManyField(Category, through='GameCategory')
+
+    def __str__(self):
+        return self.title
+
+class GameCategory(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.game.title} - {self.category.name}"
+
+class Recommendation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Recommendation for {self.user.username}: {self.game.title}"
+
+class Review(models.Model):
+    RATINGS = [(i, str(i)) for i in range(1, 6)]  # 1 a 5 estrellas
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    rating = models.IntegerField(choices=RATINGS)
+    comment = models.TextField()
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.game.title} ({self.rating}⭐)"
