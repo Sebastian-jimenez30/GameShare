206b7b107dc32fe60d49d94f3e2c51f02878e8f7
Sebastian Jimenez <anderjime864@gmail.com>
2025-03-17 22:33:49 -0500

    feat: implemented new architecture

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,3 +1,4 @@
+
 from django import forms
 
 class UserRegisterForm(forms.Form):
@@ -9,6 +10,21 @@ class UserRegisterForm(forms.Form):
     processor = forms.CharField(max_length=100)
     ram_gb = forms.IntegerField()
     graphics_card = forms.CharField(max_length=100)
+
+    def get_user_data(self):
+        return {
+            "username": self.cleaned_data["username"],
+            "email": self.cleaned_data["email"],
+            "name": self.cleaned_data["name"],
+            "password": self.cleaned_data["password"],
+            "user_type": "customer",
+            "customer": {
+                "processor": self.cleaned_data["processor"],
+                "ram_gb": self.cleaned_data["ram_gb"],
+                "graphics_card": self.cleaned_data["graphics_card"],
+            }
+        }
+
 
 class UserLoginForm(forms.Form):
     username = forms.CharField()
--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,47 +1,5 @@
 from rest_framework import serializers
 from .models import User, Customer, AdminProfile
-from apps.transactions.models import Purchase, Rental
-from apps.games.models import Game
-
-
-class CustomerNestedSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Customer
-        exclude = ['user', 'loyalty_points']
-
-
-class UserSerializer(serializers.ModelSerializer):
-    customer = CustomerNestedSerializer(required=False)
-    purchased_games = serializers.SerializerMethodField()
-    rented_games = serializers.SerializerMethodField()
-
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'name', 'password', 'customer','purchased_games','rented_games']
-        extra_kwargs = {'password': {'write_only': True}, 'email': {'required': True}}
-
-    def get_purchased_games(self, obj):
-        purchases = Purchase.objects.filter(user=obj)
-        return PurchaseSerializer(purchases, many=True).data
-
-    def get_rented_games(self, obj):
-        rentals = Rental.objects.filter(user=obj)
-        return RentalSerializer(rentals, many=True).data
-
-    def create(self, validated_data):
-        customer_data = validated_data.pop('customer', None)
-        user = User.objects.create_user(**validated_data)
-
-        if user.user_type == 'customer' and customer_data:
-            customer_data.pop('loyalty_points', None)
-            Customer.objects.create(
-                    user=user,
-                    loyalty_points=0,
-                    **customer_data
-            )
-
-        return user
-
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
@@ -53,21 +11,10 @@ class AdminProfileSerializer(serializers.ModelSerializer):
         model = AdminProfile
         fields = '__all__'
 
-class GameSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Game
-        fields = ['id', 'title', 'developer', 'year', 'price']
-
-class PurchaseSerializer(serializers.ModelSerializer):
-    game = GameSerializer()
+class UserSerializer(serializers.ModelSerializer):
+    customer = CustomerSerializer(required=False)
 
     class Meta:
-        model = Purchase
-        fields = ['game']
-
-class RentalSerializer(serializers.ModelSerializer):
-    game = GameSerializer()
-
-    class Meta:
-        model = Rental
-        fields = ['game', 'start_date', 'end_date', 'status']
+        model = User
+        fields = ['id', 'username', 'email', 'name', 'password', 'user_type', 'customer']
+        extra_kwargs = {'password': {'write_only': True}, 'email': {'required': True}}
