cc8106deb3555ebc7a14c7a050d0518fc43fe45d
Sebastian Jimenez <anderjime864@gmail.com>
2025-03-18 19:51:13 -0500

    Aply styles

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -5,6 +5,6 @@ from .views import UserRegistrationView, UserLoginView, UserLibraryView
 
 urlpatterns = [
     path('register-form/', UserRegistrationView.as_view(), name='user_register_form'),
-    path('login/', UserLoginView.as_view(), name='user_login_form'),
+    path('', UserLoginView.as_view(), name='user_login_form'),
     path('library/', UserLibraryView.as_view(), name='user_library'),
 ]
